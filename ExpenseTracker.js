628539375736c75875c3f789fa92dcea6f1f38e4
Dan <danazad@gmail.com>
2020-12-30 15:43:50 -0500

    Created a Function

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -1,142 +1,23 @@
-/*let add = document.getElementById('add');
-add.addEventListener('click', addFunction);*/
+function updateExpenses() {
+    let moneyInput = Number($("#moneyInput").val());
 
-/*$('#add').click(function() {
-    addFunction();
-});*/
+    var sum = 0;    
 
-/*let remove = document.getElementById('remove');
-remove.addEventListener('click', removeFunction);*/
-
-/*$('#remove').click(function() {
-    removeFunction();
-});*/
-
-/*function addFunction() {
-    let headerName = document.getElementById('theName');
-    let name = document.createElement('input'); 
-    name.setAttribute('placeholder', 'Name');
-    name.setAttribute('class', 'element');
-    headerName.appendChild(name);
-
-    name.addEventListener('dblclick', function() {
-        name.remove();
-        date.remove();
-        expense.remove();
-    })
-
-    let headerDate = document.getElementById('theDate');
-    let date = document.createElement('input');
-    date.setAttribute('placeHolder', 'Date');
-    date.setAttribute('class', 'element');
-    date.setAttribute('type', 'date');
-    headerDate.appendChild(date);
-
-    date.addEventListener('dblclick', function() {
-        name.remove();
-        date.remove();
-        expense.remove();
-    })
-
-    let headerExpense = document.getElementById('theExpense');
-    let expense = document.createElement('input');
-    expense.setAttribute('placeHolder', 'Expense');
-    expense.setAttribute('class', 'element expense');
-    headerExpense.appendChild(expense);
-
-    expense.addEventListener('dblclick', function() {
-        name.remove();
-        date.remove();
-        expense.remove();
-    })
-}*/
-
-/*$('#add').click(function() {
-    let headerName = $('#theName');
-    let name = $('<input>');
-    name.attr('placeholder', 'The item\'s name');
-    name.attr('class', 'element');
-    headerName.append(name);
-
-    $(name).dblclick(function() {
-        name.empty();
-        date.empty();
-        expense.empty();
+    $('.expense').each(function() { /*Adding value in a loop*/
+       sum = sum + Number($(this).val());   
     });
 
-    let headerExpense = $('#theExpense');
-    let expense = $('<input>');
-    expense.attr('placeholder', 'The item\'s expense');
-    expense.attr('class', 'element expense');
-    headerExpense.append(expense);
-
-    $('#expense').dblclick(function() {
-        name.empty();
-        date.empty();
-        expense.empty();
-    });
-
-    let headerDate = $('#theDate');
-    let date = $('<input>');
-    date.attr('placeholder', 'The item\'s date');
-    date.attr('class', 'element');
-    date.attr('type', 'date');
-    headerDate.append(date);
-
-    $(date).dblclick(function() { //?
-        name.empty();
-        date.empty();
-        expense.empty();
-    });
-});
-
-function removeFunction() {
-    let inputs = document.querySelectorAll('.element');
-    inputs.forEach(element => element.remove());
-
-    date.value = '';
-    name.value = '';
-    expense.value = '';
-};
-
-let save = document.getElementById('moneyChecker');
-let theExpense = document.getElementById('expense');
-
-let date = document.getElementById('formateDate');  //double click
-date.setAttribute('type', 'date');
-date.addEventListener('dblclick', function() {
-    date.value = '';
-});
-
-let name = document.getElementById('formateName');
-name.addEventListener('dblclick', function() {
-    name.value = '';
-});
-
-let expense = document.getElementById('formateExpense');
-expense.addEventListener('dblclick', function() {
-    expense.value = '';
-}); //double click
-
-let input = document.getElementById('moneyInput');
-input.addEventListener('input', output);
-expense.addEventListener('input', output);
-let expenses = document.querySelectorAll('.element expense');
-for (let i; i < expenses.length; i++) {
-    expenses[i].addEventListener('input', output);
+    $("#theBalance").val(moneyInput - sum);
 }
 
-function output() {
-    let outputInput = input.value;
-    let outputExpense = expense.value;
-    let outputExpenses = expenses.value;
-    let output = outputInput - outputExpense;
-    let balance = document.getElementById('theBalance');
-    balance.value = output;
-}*/
+$("#content-page").show();
+
+$("#settings-page").hide();
 
 function removeRow(element) {
     $(element).closest('tr').remove();
+    
+    updateExpenses()
 };
 
 $(document).ready(function() {
@@ -144,20 +25,21 @@ $(document).ready(function() {
         let newRow = 
         `<tr>
             <td>
-                <input class="input-name" placeholder='Name'>
+                <input onclick="remove(this)" placeholder='Name'>
             </td>
             <td>
-                <input class="input-expense" placeholder='Expense'>
-            </td>
-            <td> 
-                <input class="input-date" type='date' placeholder='Date'>
+                <input type="text" class="expense" placeholder="Expense" onkeyup="updateExpenses()">
             </td>
             <td>
-                <button onclick='removeRow(this)' class='removeRow'><span class="material-icons">
+                <input type='date' placeholder='Date'>
+            </td>
+            <td>
+                <button id="rowRemoveButton" onclick='removeRow(this)' class='removeRow'><span class="material-icons">
                 remove_circle
                 </span></button>
             </td>
         </tr>`
+
         $('#tbody').append(newRow);
     });
 
@@ -165,63 +47,30 @@ $(document).ready(function() {
         $('#tbody').empty();
     });
 
-    $('.input-name').dblclick(function(element) {
-        console.log('This works')
+    $("#moneyInput").keyup(function() {
+        updateExpenses();
+    }); 
+    
+    /*var counter = 0;   handles an event every 3 times when button releases
+    $("#moneyInput").keyup(function(){
+        counter++;
+        if(counter == 3) {
+            alert(0);
+            counter = 0;
+        }
+      });*/
+
+    $("#menu-button").click(function() {
+        $("#menu-page").slideToggle(100);
+    });
+
+    $("#settings").click(function() {
+        $("#content-page").hide("fast");
+        $("#settings-page").show("fast");
+    })
+
+    $("#content").click(function() {
+        $("#content-page").show("fast");
+        $("#settings-page").hide("fast");
     });
 });
-
-/*
-
-HINT
-
-1) add class to expense 
-2) Use "each" to calculate the sum
-
-*/
-
-/*$('#remove').click(function() {
-    let inputs = $('.element');
-    inputs.forEach(element => element.empty());
-
-    name.val('');
-    expense.val('');
-    date.val('');
-});
-
-let save = $('#moneyChecker');
-let theExpense = $('#expense');
-
-let name = $('#formateName');
-$(name).dblclick(function() {
-    name.val('');
-});
-
-let expense = $('#formateExpense');
-$(expense).dblclick(function() {
-    expense.val('');
-});
-
-let date = $("#formateDate");
-date.attr('type', 'date');
-$(date).dblclick(function() {
-    date.val('');
-});
-
-let input = $('#moneyInput');
-$(input).click('input', output);
-$(expense).click('input', output);
-let expenses = $('.element expense');
-for (let index = 0; index < expenses.length; index++) {
-    expenses[index].$(click(function() {
-        $('input,', output)
-    }));
-}
-
-$(function output() {
-    let outputInput = input.value;
-    let outputExpense = expense.value;
-    let outputExpense = expense.value;
-    let output = outputInput - outputExpense;
-    let balance = $('#theBalance');
-    balance.val(output);
-});*/
