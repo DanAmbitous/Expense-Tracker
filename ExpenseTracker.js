b19e7db7eee688204d553c29b2d5fbf6a7aa9a67
Dan <danazad@gmail.com>
2020-12-31 15:50:44 -0500

    Added a review page in the list

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -28,13 +28,18 @@ function output() {
 
 $("#content-page").show();
 
-$("#settings-page").hide();
-
 $("#income-page").hide();
 
+$("#settings-page").hide();
+
+$("#rating-page").hide();
+
 function removeRow(element) { //Row remove button
-    $(element).closest("tr").remove();
-    confirm("Are you sure that you want to clear the row?");
+    if (confirm("Are you sure that you want to clear the row?") == true) {
+        $(element).closest("tr").remove();
+    } else {
+        return false
+    }
 };
 
 $(document).ready(function() {
@@ -87,13 +92,19 @@ $(document).ready(function() {
     });
 
     $("#removeExpense").click(function() { //Remove button
-        $(".removeExpense").remove();
-        confirm("Are you sure that you want to clear the expense list?");
+        if(confirm("Are you sure that you want to clear the expense list?") == true) {
+            $(".removeExpense").remove();
+        } else {
+            return false
+        }
     });
 
     $("#removeIncome").click(function() { //Remove button
-        $(".removeIncome").remove();
-        confirm("Are you sure that you want to clear the income list?");
+        if (confirm("Are you sure that you want to clear the income list?") == true) {
+            $(".removeIncome").remove();
+        } else {
+            return false
+        }
     });
 
     $("#moneyInput").keyup(function() {
@@ -101,36 +112,121 @@ $(document).ready(function() {
     });
 
     $("#incomes").click(function() {
-        $("#table-incomes").show();
-        $("#expenses").show();
-        $("#table-expenses").hide();
-        $("#incomes").hide();
-        $("#removeExpense").hide();
-        $("#removeIncome").show();
-        $("#add-expense").hide();
-        $("#add-income").show();
+        $("#table-incomes").show(250);
+        $("#expenses").show(250);
+        $("#table-expenses").hide(250);
+        $("#incomes").hide(250);
+        $("#removeExpense").hide(250);
+        $("#removeIncome").show(250);
+        $("#add-expense").hide(250);
+        $("#add-income").show(250);
     });
 
     $("#expenses").click(function() { //Expenses page
-        $("#table-incomes").hide();
-        $("#expenses").hide();
-        $("#incomes").show();
-        $("#table-expenses").show(); 
-        $("#add-expense").show();
-        $("#add-income").hide();
+        $("#table-incomes").hide(250);
+        $("#expenses").hide(250);
+        $("#table-expenses").show(250);
+        $("#incomes").show(250);
+        $("#removeExpense").show(250);
+        $("#removeIncome").hide(250);
+        $("#table-expenses").show(250); 
+        $("#add-expense").show(250);
+        $("#add-income").hide(250);
     });
 
     $("#light-mode").click(function() {
         $("body").css('background-color', 'white');
         $("h1, p").css('color', 'black');
         $("#menu").css('color', 'black');
+        $("#rating-page").css('color', 'black');
     });
 
     $("#dark-mode").click(function() {
         $("body").css('background-color', 'black');
         $("h1, p").css('color', 'white');
         $("#menu").css('color', 'white');
+        $("#rating-page").css('color', 'white');
     });
+
+    //Stars
+    $("#star-button-1").mouseover(function() {
+        $(this).css('color', 'yellow');
+    });
+
+    $("#star-button-1").mouseout(function() {
+        $(this).css('color', 'blue');
+    });
+
+    $("#star-button-2").mouseover(function() {
+        $("#star-button-1").css('color', 'yellow');
+        $(this).css('color', 'yellow');
+    });
+
+    $("#star-button-2").mouseout(function() {
+        $("#star-button-1").css('color', 'blue');
+        $(this).css('color', 'blue');
+    });
+
+    $("#star-button-3").mouseover(function() {
+        $("#star-button-1").css('color', 'yellow');
+        $("#star-button-2").css('color', 'yellow');
+        $(this).css('color', 'yellow');
+    });
+
+    $("#star-button-3").mouseout(function() {
+        $("#star-button-1").css('color', 'blue');
+        $("#star-button-2").css('color', 'blue');
+        $(this).css('color', 'blue');
+    });
+
+    $("#star-button-4").mouseover(function() {
+        $("#star-button-1").css('color', 'yellow');
+        $("#star-button-2").css('color', 'yellow');
+        $("#star-button-3").css('color', 'yellow');
+        $(this).css('color', 'yellow');
+    });
+
+    $("#star-button-4").mouseout(function() {
+        $("#star-button-1").css('color', 'blue');
+        $("#star-button-2").css('color', 'blue');
+        $("#star-button-3").css('color', 'blue');
+        $(this).css('color', 'blue');
+    });
+
+    $("#star-button-5").mouseover(function() {
+        $("#star-button-1").css('color', 'yellow');
+        $("#star-button-2").css('color', 'yellow');
+        $("#star-button-3").css('color', 'yellow');
+        $("#star-button-4").css('color', 'yellow');
+        $(this).css('color', 'yellow');
+    });
+
+    $("#star-button-5").mouseout(function() {
+        $("#star-button-1").css('color', 'blue');
+        $("#star-button-2").css('color', 'blue');
+        $("#star-button-3").css('color', 'blue');
+        $("#star-button-4").css('color', 'blue');
+        $(this).css('color', 'blue');
+    });
+
+    $("#star-button-6").mouseover(function() {
+        $("#star-button-1").css('color', 'yellow');
+        $("#star-button-2").css('color', 'yellow');
+        $("#star-button-3").css('color', 'yellow');
+        $("#star-button-4").css('color', 'yellow');
+        $("#star-button-5").css('color', 'yellow');
+        $(this).css('color', 'yellow');
+    });
+
+    $("#star-button-6").mouseout(function() {
+        $("#star-button-1").css('color', 'blue');
+        $("#star-button-2").css('color', 'blue');
+        $("#star-button-3").css('color', 'blue');
+        $("#star-button-4").css('color', 'blue');
+        $("#star-button-5").css('color', 'blue');
+        $(this).css('color', 'blue');
+    });
+    //Stars
 
     $("#menu-button").click(function() {
         $("#menu-page").slideToggle(100);
@@ -139,10 +235,18 @@ $(document).ready(function() {
     $("#settings").click(function() {
         $("#content-page").hide("fast");
         $("#settings-page").show("fast");
+        $("#rating-page").hide("fast");
     })
+
+    $("#rating").click(function() {
+        $("#content-page").hide("fast");
+        $("#settings-page").hide("fast");
+        $("#rating-page").show("fast");    
+    });
 
     $("#content").click(function() {
         $("#content-page").show("fast");
         $("#settings-page").hide("fast");
+        $("#rating-page").hide("fast");
     });
 });
