4296f3a66e695f52626d4a4002f15686f1f36d1c
Dan <danazad@gmail.com>
2020-12-31 00:04:33 -0500

    Added income section in code

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -10,6 +10,18 @@ function expenseInput() {
     $("#theBalance").val(input - sum);
 };
 
+function incomeInput() {
+    let input = Number($("#moneyInput").val());
+
+    var sum = 0;
+
+    $(".income").each(function() {
+        sum = sum + Number($(this).val());
+    });
+
+    $("#theBalance").val(input + sum);
+}
+
 $("#content-page").show();
 
 $("#settings-page").hide();
@@ -24,55 +36,52 @@ function removeRow(element) { //Row remove button
 
 $(document).ready(function() {
     $("#add").click(function() {
-    var rowExpense = 
-        `
-        <tr>
-            <td>
-                <input type="text" placeholder="The item's name">
-            </td>
-            <td>
-                <input type="number" class="expense" placeholder="Item's cost" onkeyup="expenseInput()">
-            </td>
-            <td>
-                <input type="date">
-            </td>
-            <td>
-                <button id="rowRemoveButton" onclick="removeRow(this)" class="removeRow"><span class="material-icons">
-                    remove_circle
-                </span></button>
-            </td>
-        </tr>
-        `
-    var rowIncome = 
-        `
-        <tr>
-            <td>
-                <input type="text" placeholder="The item's name">
-            </td>
-            <td>
-                <input type="number" class="expense" placeholder="Income" onkeyup="expenseInput()">
-            </td>
-            <td>
-                <input type="date">
-            </td>
-            <td>
-                <button id="rowRemoveButton" onclick="removeRow(this)" class="removeRow"><span class="material-icons">
-                    remove_circle
-                </span></button>
-            </td>
-        </tr>
-        `
+        var rowExpense = 
+            `
+            <tr class="removeTr">
+                <td>
+                    <input type="text" placeholder="The item's name">
+                </td>
+                <td>
+                    <input type="number" class="expense" placeholder="Item's cost" onkeyup="expenseInput()">
+                </td>
+                <td>
+                    <input type="date">
+                </td>
+                <td>
+                    <button id="rowRemoveButton" onclick="removeRow(this)" class="removeRow"><span class="material-icons">
+                        remove_circle
+                    </span></button>
+                </td>
+            </tr>
+            `
+        var rowIncome = 
+            `
+            <tr class="removeTr">
+                <td>
+                    <input type="text" placeholder="The item's name">
+                </td>
+                <td>
+                    <input type="number" class="income" placeholder="Income" onkeyup="incomeInput()">
+                </td>
+                <td>
+                    <input type="date">
+                </td>
+                <td>
+                    <button id="rowRemoveButton" onclick="removeRow(this)" class="removeRow"><span class="material-icons">
+                        remove_circle
+                    </span></button>
+                </td>
+            </tr>
+            `
 
         $("#table-expenses").append(rowExpense);
         $("#table-incomes").append(rowIncome);
     });
 
     $("#remove").click(function() { //Remove button
-        $("tr").remove();
-    });
-
-    $("#moneyInput").keyup(function() { //Wallet input
-        expenseInput();
+        $(".expenseTr").remove();
+        confirm("Are you sure that you want to clear the list?");
     });
 
     $("#incomes").click(function() {
@@ -87,6 +96,10 @@ $(document).ready(function() {
         $("#expenses").hide();
         $("#incomes").show();
         $("#table-expenses").show(); 
+    });
+
+    $("#moneyInput").keyup(function() { //Wallet input
+        expenseInput();
     });
 
     $("#menu-button").click(function() {
